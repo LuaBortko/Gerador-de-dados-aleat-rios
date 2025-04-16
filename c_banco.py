bf75ea20ae7bece1dce2edec705711d4c21c71cc
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2025-04-16 00:17:03 -0300

    resetar e criar a tabela estão n oproprio codigo

--- c_banco.py
+++ c_banco.py
@@ -83,17 +83,6 @@ fake.add_provider(disciplinas)
 fake.add_provider(periodos)
 fake.add_provider(semestres)
 
-##depart = {"id": None, "nome": None, "ra_coordenador": None}
-##prof = {"ra": None, "nome": None, "id_depart": None}
-##curs = {"id": None, "nome": None, "ra_coordenador": None}
-##alun = {"ra": None, "nome": None, "id_curso": None}
-##discip = {"id": None, "nome": None, "id_depart": None, "id_curso": None, "ra_coordenador": None}
-##turm = {"id": None, "id_disciplina": None, "semestre": None, "ano": None, "periodo": None, "ra_professor": None}
-##matri_cur = {"id_curso": None, "id_disciplina": None}
-#hist = {"ra_aluno": None, "id_disciplina": None, "id_turma": None, "nota": None}
-#tc_c = {"id": None, "nome": None, "ra_professor": None}
-#tcc_alun = {"id_tcc": None, "ra_aluno": None}
-
 def gerarDepartamento(n):
     deptos = []
     nomes = []
@@ -295,120 +284,6 @@ def gerarTCC_aluno(tcc,aluno):
     tcc_alun = {"id_tcc": tcc, "ra_aluno": aluno}
     return tcc_alun
 
-def resetarDB():
-    cursor.execute('alter table departamento DROP constraint ra ' \
-    'drop table if exists historico;' \
-    'drop table if exists matriz_curricular;' \
-    'drop table if exists tcc_aluno;' \
-    'drop table if exists tcc;' \
-    'drop table if exists turma;' \
-    'drop table if exists disciplina;' \
-    'drop table if exists aluno;' \
-    'drop table if exists curso;' \
-    'drop table if exists professor;' \
-    'drop table if exists departamento;')
-    cursor.execute("commit")
-
-def criarDB():
-    cursor.execute("""
-  create table departamento(
-    id varchar(6)
-    ,ra_professor varchar(15)
-    ,nome varchar(30)
-    ,primary key (id)
-  );
-
-   create table professor(
-    ra varchar(15)
-    ,nome varchar(30)
-    ,id_departamento varchar(6)
-    ,primary key (ra)
-    ,foreign key (id_departamento) references departamento(id)
-
-  );
-
-  create table curso(
-    id varchar(6)
-    ,nome varchar(30)
-    ,ra_coordenador varchar(15)
-    ,primary key (id)
-    ,foreign key (ra_coordenador) references professor(ra)
-  );
-
-create table aluno(
-  ra varchar(15),
-  id_curso varchar(6),
-  nome varchar(30),
-  primary key (ra)
-  ,foreign key (id_curso) references curso(id)
-  );
-
-  create table disciplina(
-  id varchar(6)
-  ,nome varchar(50)
-  ,id_departamento varchar(6)
-  ,id_curso varchar(6)
-  ,ra_coordenador varchar(15)
-  ,primary key (id)
-  ,foreign key (id_departamento) references departamento(id)
-  ,foreign key (id_curso) references curso(id)
-  ,foreign key (ra_coordenador) references professor(ra) 
-);
-
-create table turma(
-  id varchar(3)
-  ,id_disciplina varchar(6)
-  ,semestre varchar(10)
-  ,ano varchar(4)
-  ,periodo varchar(10)
-  ,ra_professor varchar(15)
-  ,primary key (id)
-  ,foreign key (ra_professor) references professor(ra)
-);
-
-create table TCC(
-  nome varchar(50)
-  ,ra_professor varchar(15)
-  ,id varchar(6)
-  ,primary key (id)
-  ,foreign key (ra_professor) references professor(ra)
-);
-
-create table TCC_aluno(
-  id_tcc varchar(6)
-  ,ra_aluno varchar(15)
-  ,foreign key (id_tcc) references TCC(id)
-  ,foreign key (ra_aluno) references aluno(ra)
-);
-
-create table matriz_curricular(
-  id_curso varchar(6)
-  ,id_disciplina varchar(6)
-  ,foreign key (id_curso) references curso(id)
-  ,foreign key (id_disciplina) references disciplina(id)
-);
-
-create table historico(
-  ra_aluno varchar(15)
-  ,id_disciplina varchar(6)
-  ,id_turma varchar(3)
-  ,nota numeric(4,2)
-  ,foreign key (ra_aluno) references aluno(ra)
-  ,foreign key (id_disciplina) references disciplina(id)
-  ,foreign key (id_turma) references turma(id)
-);
-
-
-
-ALTER TABLE matriz_curricular
-ADD COLUMN semestre varchar;
-
-ALTER TABLE departamento
-ADD CONSTRAINT ra FOREIGN KEY (ra_professor)
-REFERENCES professor(ra);
-    """)
-    cursor.execute("commit")
-
 import psycopg2
 from dotenv import load_dotenv
 import os
@@ -476,8 +351,125 @@ try:
         query = "UPDATE departamento SET ra_professor = '" + dicionario["ra_coordenador"] + "' WHERE id = '" + dicionario["id"] + "'"
         cursor.execute(query)
         cursor.execute("commit")
+    def resetarDB():
+        cursor.execute('ALTER TABLE departamento DROP CONSTRAINT IF EXISTS ra;')
+        tabelas = [
+        'historico',
+        'matriz_curricular',
+        'tcc_aluno',
+        'tcc',
+        'turma',
+        'disciplina',
+        'aluno',
+        'curso',
+        'professor',
+        'departamento'
+        ]
+        for tabela in tabelas:
+            cursor.execute(f'DROP TABLE IF EXISTS {tabela} CASCADE;')
+        cursor.execute("commit")
+    def criarDB():
+        cursor.execute("""
+        create table departamento(
+            id varchar(6)
+            ,ra_professor varchar(15)
+            ,nome varchar(30)
+            ,primary key (id)
+        );
+
+        create table professor(
+            ra varchar(15)
+            ,nome varchar(30)
+            ,id_departamento varchar(6)
+            ,primary key (ra)
+            ,foreign key (id_departamento) references departamento(id)
+
+        );
+
+        create table curso(
+            id varchar(6)
+            ,nome varchar(30)
+            ,ra_coordenador varchar(15)
+            ,primary key (id)
+            ,foreign key (ra_coordenador) references professor(ra)
+        );
+
+        create table aluno(
+        ra varchar(15),
+        id_curso varchar(6),
+        nome varchar(30),
+        primary key (ra)
+        ,foreign key (id_curso) references curso(id)
+        );
+
+        create table disciplina(
+        id varchar(6)
+        ,nome varchar(50)
+        ,id_departamento varchar(6)
+        ,id_curso varchar(6)
+        ,ra_coordenador varchar(15)
+        ,primary key (id)
+        ,foreign key (id_departamento) references departamento(id)
+        ,foreign key (id_curso) references curso(id)
+        ,foreign key (ra_coordenador) references professor(ra) 
+        );
+
+        create table turma(
+        id varchar(10)
+        ,id_disciplina varchar(6)
+        ,semestre varchar(10)
+        ,ano varchar(4)
+        ,periodo varchar(10)
+        ,ra_professor varchar(15)
+        ,primary key (id)
+        ,foreign key (ra_professor) references professor(ra)
+        );
+
+        create table TCC(
+        nome varchar(150)
+        ,ra_professor varchar(15)
+        ,id varchar(10)
+        ,primary key (id)
+        ,foreign key (ra_professor) references professor(ra)
+        );
+
+        create table TCC_aluno(
+        id_tcc varchar(10)
+        ,ra_aluno varchar(15)
+        ,foreign key (id_tcc) references TCC(id)
+        ,foreign key (ra_aluno) references aluno(ra)
+        );
+
+        create table matriz_curricular(
+        id_curso varchar(6)
+        ,id_disciplina varchar(6)
+        ,foreign key (id_curso) references curso(id)
+        ,foreign key (id_disciplina) references disciplina(id)
+        );
+
+        create table historico(
+        ra_aluno varchar(15)
+        ,id_disciplina varchar(6)
+        ,id_turma varchar(10)
+        ,nota varchar(10)
+        ,foreign key (ra_aluno) references aluno(ra)
+        ,foreign key (id_disciplina) references disciplina(id)
+        ,foreign key (id_turma) references turma(id)
+        );
+
+        ALTER TABLE matriz_curricular
+        ADD COLUMN semestre varchar;
+
+        ALTER TABLE departamento
+        ADD CONSTRAINT ra FOREIGN KEY (ra_professor)
+        REFERENCES professor(ra);
+        """)
+        cursor.execute("commit")
+
+
     #Main
-    
+    resetarDB()
+    criarDB()
     n = randint(3,8)
     #gera o numero de departamentos e professores
     l_aux = []
